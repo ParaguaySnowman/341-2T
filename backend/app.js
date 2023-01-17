c6311748d8358f6a3fef96224f64169030f2508b
Parker Mortensen <parker.b.mortensen@gmail.com>
2023-01-16 21:55:42 -0700

    everything except image ready

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const professionalRoutes = require('./routes/professional');
+const bodyParser = require('body-parser');
 
-app.use('/', require('./routes'))
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/professional', professionalRoutes);
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
